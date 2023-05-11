554beb2659d6c67b712d40f6428f0d1f17015562
Andriy Tsvok <vpupkin@company.com>
2023-05-11 18:19:22 +0300

    divided into separate widgets and improved some code

--- lib/screens/bottom_bar_screen/bottom_bar_screen.dart
+++ lib/screens/bottom_bar_screen/bottom_bar_screen.dart
@@ -1,11 +1,11 @@
+import 'package:chat_ui_test_app/screens/bottom_bar_screen/widgets/bottom_bar_widget.dart';
 import 'package:flutter/material.dart';
 import '../chat_screen/chat_screen.dart';
 import '../more_screen/more_screen.dart';
 import '../notifications_screen/notifications_screen.dart';
-import 'package:custom_line_indicator_bottom_navbar/custom_line_indicator_bottom_navbar.dart';
 
 class BottomBarScreen extends StatefulWidget {
-  const BottomBarScreen({super.key});
+  const BottomBarScreen({Key? key}) : super(key: key);
 
   @override
   BottomBarScreenState createState() => BottomBarScreenState();
@@ -15,9 +15,9 @@ class BottomBarScreenState extends State<BottomBarScreen> {
   int _selectedIndex = 0; //default index
 
   final List<Widget> widgetOptions = [
-    const ChatScreen(),
-    const NotificationScreen(),
-    const MoreScreen(),
+     const ChatScreen(),
+     const NotificationScreen(),
+     const MoreScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -32,33 +32,9 @@ class BottomBarScreenState extends State<BottomBarScreen> {
       body: Center(
         child: widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: CustomLineIndicatorBottomNavbar(
-        selectedColor: const Color(0xfff25276),
-        // Modified selected color
-        unSelectedColor: const Color(0xff06293e),
-        // Modified unselected color
-        backgroundColor: Colors.white,
-        currentIndex: _selectedIndex,
-        unselectedIconSize: 28,
-        selectedIconSize: 33,
-        enableLineIndicator: true,
-        lineIndicatorWidth: 3,
-        indicatorType: IndicatorType.Top,
-        customBottomBarItems: [
-          CustomBottomBarItems(
-            label: 'Chat',
-            icon: Icons.wechat,
-          ),
-          CustomBottomBarItems(
-            label: 'Notifications',
-            icon: Icons.notifications_none,
-          ),
-          CustomBottomBarItems(
-            label: 'More',
-            icon: Icons.more_horiz,
-          ),
-        ],
-        onTap: _onItemTapped, // Call the function here
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/screens/chat_screen/chat_screen.dart
+++ lib/screens/chat_screen/chat_screen.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
-import '../../models/messages.dart';
 import 'package:chat_ui_test_app/screens/chat_screen/widgets/chat_app_bar.dart';
+import 'package:chat_ui_test_app/screens/chat_screen/widgets/chat_list_widget.dart';
 import 'package:chat_ui_test_app/screens/chat_screen/widgets/chat_search_bar.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_ui_test_app/models/messages.dart';
+
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -41,82 +42,7 @@ class ChatScreenState extends State<ChatScreen> {
               child: ChatSearchBar(onChanged: filterMessages),
             ),
             Expanded(
-              child: ListView.separated(
-                itemCount: filteredMessages.length,
-                separatorBuilder: (context, index) => const Divider(),
-                itemBuilder: (context, index) {
-                  final message = filteredMessages[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(message.image),
-                      radius: 28.0,
-                    ),
-                    title: Text(
-                      message.name,
-                      style: GoogleFonts.lato(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          message.message,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(height: 5.0),
-                        if (message.category.isNotEmpty)
-                          Container(
-                            decoration: BoxDecoration(
-                              color: message.contColor,
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 6.0,
-                              vertical: 2.0,
-                            ),
-                            child: Text(
-                              message.category,
-                              style: const TextStyle(
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                      ],
-                    ),
-                    trailing: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          message.time,
-                          style: const TextStyle(
-                            fontSize: 12.0,
-                            color: Color(0xFF507B96),
-                          ),
-                        ),
-                        const SizedBox(height: 5.0),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Color(0xfff25276),
-                            shape: BoxShape.circle,
-                          ),
-                          padding: const EdgeInsets.all(5.0),
-                          child: Text(
-                            message.numOfMsg,
-                            style: const TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+              child: ChatList(messages: filteredMessages),
             ),
           ],
         ),
@@ -124,3 +50,5 @@ class ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+
