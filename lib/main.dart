1958a8e8d0d789954848602806780f9f01c130aa
Andriy Tsvok <vpupkin@company.com>
2023-05-11 16:29:06 +0300

    add assets and fonts add chat screen( 2 wigdets)

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: BottomBarScreen(),
     );
   }
